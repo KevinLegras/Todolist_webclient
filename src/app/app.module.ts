a8e2d85ac63e0a894f4e79c385d3cbd835e0ca63
Kevin <legraskevin@outlook.com>
2021-12-05 15:10:09 +0100

    :package: Import package cdk/drap-drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     TodoItemComponent
-    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
