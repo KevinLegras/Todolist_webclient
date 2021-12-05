44481a1868fdb1979c8126cdc7f4a26b57832d56
Kevin <legraskevin@outlook.com>
2021-12-05 18:01:12 +0100

    Create new component datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,33 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+
+//drag&drop
 import { DragDropModule } from '@angular/cdk/drag-drop';
+
+//datepicker
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DatePickerComponent } from './date-picker/date-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    DatePickerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
     DragDropModule
   ],
   providers: [],
